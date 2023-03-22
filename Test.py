21977033cf665e0aeb360d657e091fc742b91b64
MASSONCamille <masson.camille@ymail.com>
2023-03-22 02:08:15 +0100

    test

---
+++ Test.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+import math
+
+class HexagonGridCanvas(tk.Canvas):
+    def __init__(self, master, side_length=50, rows=5, cols=5, **kwargs):
+        super().__init__(master, **kwargs)
+        self.side_length = side_length
+        self.rows = rows
+        self.cols = cols
+
+    def axial_to_pixel(self, q, r):
+        x = self.winfo_width() / 2 + self.side_length * math.sqrt(3) * (q + r/2) # Calcul des coordonnées x en utilisant les coordonnées axiales
+        y = self.winfo_height() / 2 + self.side_length * 1.5 * r # Calcul des coordonnées y en utilisant les coordonnées axiales
+        return x, y
+
+    def draw_hexagon_grid(self):
+        for q in range(-self.cols//2, self.cols//2+1):
+            for r in range(-self.rows//2, self.rows//2+1):
+                x_center, y_center = self.axial_to_pixel(q, r)
+                points = []
+                for i in range(6):
+                    angle_rad = math.pi / 3 * (i + 0.5)
+                    x = x_center + self.side_length * math.cos(angle_rad)
+                    y = y_center + self.side_length * math.sin(angle_rad)
+                    points.extend([x, y])
+                self.create_polygon(points, fill='blue')
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    canvas = HexagonGridCanvas(root, side_length=30, rows=5, cols=5, width=500, height=500)
+    canvas.pack()
+
+    root.update()
+    canvas.draw_hexagon_grid()
+
+    root.mainloop()
--- view/HexDraw.py
+++ view/HexDraw.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from math import cos, sin, sqrt, radians
 
+
 class HexDraw:
-    def __init__(self, x ,y ) -> None:
+    def __init__(self, x, y) -> None:
         self.x: int = x
         self.y: int = y
